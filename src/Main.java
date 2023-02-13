fc433cce06a2bb4c9fba7c5d30c7278f7f7f644a
ppgllrd <ppgllrd@gmail.com>
2023-02-13 19:43:29 +0100

    Add class for generating a random scenario.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import automata.CellularAutomaton;
 import automata.CellularAutomatonParameters;
-import automata.Scenario;
 import automata.Statistics;
 import automata.neighbourhood.MooreNeighbourhood;
 import automata.pedestrian.PedestrianParameters;
-import geometry._2d.Rectangle;
 
 import static statistics.Random.random;
 
@@ -20,48 +18,9 @@ class Main {
 
     int rows = 40, columns = 120;
     var cellDimension = 0.4; // 0.4 meters
-    var scenario = new Scenario(rows, columns, cellDimension);
+    var scenario = new RandomScenario(rows, columns, cellDimension);
 
-    // place exits
-    if (random.bernoulli(0.9)) {
-      scenario.setExit(new Rectangle(2, columns - 1, 5, 1));
-    }
-    if (random.bernoulli(0.9)) {
-      scenario.setExit(new Rectangle(rows - 7, columns - 1, 5, 1));
-    }
-    if (random.bernoulli(0.9)) {
-      scenario.setExit(new Rectangle(10, 0, 5, 1));
-    }
-    if (random.bernoulli(0.9)) {
-      scenario.setExit(new Rectangle(rows - 15, 0, 5, 1));
-    }
-    if (random.bernoulli(0.5)) {
-      scenario.setExit(new Rectangle(rows / 2, columns / 2, 2, 2));
-    }
-
-    // place blocks
-    int numberOfBlocks = random.nextInt(20, 50);
-    int numberOfBlocksPlaced = 0;
-    while (numberOfBlocksPlaced < numberOfBlocks) {
-      var width = 1 + random.nextInt(20);
-      var height = 1 + random.nextInt(Math.max(1, rows / (2 * width)));
-
-      var row = random.nextInt(0, 1 + rows - height);
-      var column = random.nextInt(3, 1 + columns - width - 3);
-
-      var newBlock = new Rectangle(row, column, height, width);
-      var border = new Rectangle(row - 2, column - 2, height + 4, width + 4);
-
-      var shoulBePlaced = !border.intersects(scenario.exits())
-          && !border.intersects(scenario.blocks());
-
-      if (shoulBePlaced) {
-        scenario.setBlock(newBlock);
-        numberOfBlocksPlaced++;
-      }
-    }
-
-    var parameters =
+    var cellularAutomatonParameters =
         CellularAutomatonParameters.Builder
             .scenario(scenario)
             .secondsTimeLimit(60 * 10) // 10 minutes
@@ -70,7 +29,7 @@ class Main {
             .GUITimeFactor(25) // x25 times faster
             .build();
 
-    var automaton = new CellularAutomaton(parameters);
+    var automaton = new CellularAutomaton(cellularAutomatonParameters);
 
     // place pedestrians
     var numberOfPedestrians = random.nextInt(300, 600);
@@ -83,9 +42,8 @@ class Main {
             .build();
     automaton.addPedestriansUniformly(numberOfPedestrians, pedestrianParameters);
 
-    automaton.runGUI();
+    automaton.runGUI(); // automaton.run() to run without GUI
     Statistics statistics = automaton.computeStatistics();
     System.out.println(statistics);
-    // automaton.run();
   }
 }
---
+++ src/RandomScenario.java
@@ -0,0 +1,54 @@
+import automata.Scenario;
+import geometry._2d.Rectangle;
+
+import static statistics.Random.random;
+
+/**
+ * A class for generating random scenarios
+ * @author Pepe Gallardo
+ */
+public class RandomScenario extends Scenario {
+  public RandomScenario(int rows, int columns, double cellDimension) {
+    super(rows, columns, cellDimension);
+
+    // place exits
+    if (random.bernoulli(0.9)) {
+      setExit(new Rectangle(2, columns - 1, 5, 1));
+    }
+    if (random.bernoulli(0.9)) {
+      setExit(new Rectangle(rows - 7, columns - 1, 5, 1));
+    }
+    if (random.bernoulli(0.9)) {
+      setExit(new Rectangle(10, 0, 5, 1));
+    }
+    if (random.bernoulli(0.9)) {
+      setExit(new Rectangle(rows - 15, 0, 5, 1));
+    }
+    if (random.bernoulli(0.5)) {
+      setExit(new Rectangle(rows / 2, columns / 2, 2, 2));
+    }
+
+    // place blocks
+    int numberOfBlocks = random.nextInt(20, 50);
+    int numberOfBlocksPlaced = 0;
+    while (numberOfBlocksPlaced < numberOfBlocks) {
+      var width = 1 + random.nextInt(20);
+      var height = 1 + random.nextInt(Math.max(1, rows / (2 * width)));
+
+      var row = random.nextInt(0, 1 + rows - height);
+      var column = random.nextInt(3, 1 + columns - width - 3);
+
+      var newBlock = new Rectangle(row, column, height, width);
+      // so that blocks are apart
+      var border = new Rectangle(row - 2, column - 2, height + 4, width + 4);
+
+      var shoulBePlaced = !border.intersects(exits())
+          && !border.intersects(blocks());
+
+      if (shoulBePlaced) {
+        setBlock(newBlock);
+        numberOfBlocksPlaced++;
+      }
+    }
+  }
+}
